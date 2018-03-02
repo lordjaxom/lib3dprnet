b8541d57d5383543944296539c095ea9cdef7c5c
Sascha Volkenandt <lordjaxom@speedstar>
2018-03-02 21:14:05 +0100

    adjustments for linux

--- core/config.hpp
+++ core/config.hpp
@@ -1,10 +1,14 @@
 #ifndef LIB3DPRNET_CORE_CONFIG_HPP
 #define LIB3DPRNET_CORE_CONFIG_HPP
 
-#if defined( _3dprnet_EXPORTS )
-#   define PRNET_DLL __declspec( dllexport )
+#if defined( WIN32 )
+#   if defined( _3dprnet_EXPORTS )
+#       define PRNET_DLL __declspec( dllexport )
+#   else
+#       define PRNET_DLL __declspec( dllimport )
+#   endif
 #else
-#   define PRNET_DLL __declspec( dllimport )
+#   define PRNET_DLL
 #endif
 
 #endif //LIB3DPRNET_CORE_CONFIG_HPP
